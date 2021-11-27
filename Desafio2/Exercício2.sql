ba8540c767e36f8e5dd202b52d78bb2f4da9e96f
Thomas Couto <95130249+thomasgocouto@users.noreply.github.com>
2021-11-27 15:03:58 -0300

    Update Exercício2.sql

--- Desafio2/Exercício2.sql
+++ Desafio2/Exercício2.sql
@@ -2,17 +2,17 @@
 qual foi o valor total de todas as compras deles, quantidade de compras, e 
 valor médio gasto por compras, ordene os mesmos por ordem decrescente pela media do valor de compra.
 */
---Selecionar: C.customer_id, COUNT(O.order_id), SUM(OI.price), AVG(OI.price)
---Ordenar por: AVG(OI.price) DESC
+--Selecionar: C.customer_id, COUNT(O.order_id), SUM(OP.payment_value), AVG(OP.payment_value)
+--Ordenar por: AVG(OP.payment_value) DESC
 
 SELECT
      C.customer_id AS CustomerID
      , 
      COUNT(O.order_id) AS OrderCount
      , 
-     SUM(OI.price) AS TotalSpend
+     SUM(OP.payment_value) AS TotalSpend
      , 
-     AVG(OI.price) AS AverageSpend
+     AVG(OP.payment_value) AS AverageSpend
     
 FROM
     `summer-drive-329021.OlistDataset.olist_customers_dataset` C
@@ -21,11 +21,11 @@ INNER JOIN
 ON 
     C.customer_id=O.customer_id
 INNER JOIN 
-    `summer-drive-329021.OlistDataset.olist_order_items_dataset` OI
+    `summer-drive-329021.OlistDataset.olist_order_payments_dataset` OP
 ON 
-    O.order_id=OI.order_id
+    O.order_id=OP.order_id
 GROUP BY 
     C.customer_id
 ORDER BY 
-    AVG(OI.price) DESC
+    AVG(OI.payment_value) DESC
 LIMIT 5
